36b33dc2ec3886b18f85374e18d3a46c0bfeade3
Geovanna Duarte <geovannaduarttes@gmail.com>
2022-05-18 19:00:17 -0300

    Exercícios Alura

---
+++ sintaxe-variaveis-e-fluxo/MultiplosDeTres.java
@@ -0,0 +1,12 @@
+
+public class MultiplosDeTres {
+
+	public static void main(String[] args) {
+		
+		for(int multiplo = 3; multiplo < 100; multiplo = multiplo + 3) {
+			
+			System.out.println(multiplo);
+			
+		}
+	}
+}
